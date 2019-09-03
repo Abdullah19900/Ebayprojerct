55db89045ec9a8b8b557539cc3e442f83d8932d6
aij19 <aij19@10.67.110.89>
2019-09-03 19:21:31 -0400

    first commit

---
+++ Ebayprojerct/src/test/java/com/common/Base23.java
@@ -0,0 +1,40 @@
+package com.common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base23 {
+
+
+	public WebDriver driver;
+
+	public WebDriver getDriver() {
+
+		System.setProperty("webdriver.gecko.driver", "./Browser/geckodriver");
+		driver = new FirefoxDriver();
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return driver;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
